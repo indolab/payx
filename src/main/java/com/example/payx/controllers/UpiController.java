7904cd43a3d9a9da599fbc89e3503fd82ec883f9
indolab <suyashm480@gmail.com>
2025-01-16 08:07:46 +0530

    modified:   src/main/java/com/example/payx/controllers/UpiController.java 	modified:   src/main/java/com/example/payx/services/UserService.java 	modified:   src/main/resources/templates/profile.html 	modified:   src/main/resources/templates/upi.html

--- src/main/java/com/example/payx/controllers/UpiController.java
+++ src/main/java/com/example/payx/controllers/UpiController.java
@@ -1,10 +1,29 @@
 package com.example.payx.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.payx.services.UserService;
+
 
 @Controller
 public class UpiController {
+    @Autowired
+    UserService usrsr;
+    @GetMapping("/upi")
+    public String upipage() {
+        return "upi";
+    }
+    
+@PostMapping("/upi")
+public String upimoney(@RequestParam Integer upiid ,@RequestParam Integer balance ) {
+    usrsr.upipayment(upiid, balance);
 
+    return "upi";
+}
 
     
 }
--- src/main/java/com/example/payx/services/UserService.java
+++ src/main/java/com/example/payx/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.payx.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import com.example.payx.models.User;
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+    
     private User user ;
 
     //private Integer pool = 1000;
@@ -36,6 +39,29 @@ public class UserService {
             return true;
         }
         else{
+            return false;
+        }
+
+    }
+    public boolean upipayment(Integer id , Integer bal){
+
+        User sender = userRepo.findByEmail(curr);
+        int sender_curr_bal =sender.getBalance();
+        Optional<User> reciver =userRepo.findById(id);
+
+        if(sender_curr_bal != 0){
+            sender.setBalance(sender_curr_bal-bal);
+            userRepo.save(sender);
+
+            reciver.ifPresent(rec->{
+            rec.setBalance(bal+rec.getBalance());
+            userRepo.save(rec);
+            });
+            
+            return true;
+        }
+        else{
+            System.out.println("Sender has no balance");
             return false;
         }
 
